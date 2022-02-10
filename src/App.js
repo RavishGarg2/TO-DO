886c888b1fcdcf89a5443ab8201a251169e331a2
Ravish Kumar garg <ravishgarg8@gmail.com>
2022-02-10 08:56:50 +0530

    Title Change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   }
   return (
     <div className="App">
-    <h1 style={{color:'#05386B', textAlign:'center'}}> To-DO List</h1>
+    <h1 style={{color:'#05386B', textAlign:'center'}}> To-DO LISTS</h1>
     <div className="input-group mb-3">
   <input type="text" onChange={eventHandler} value={data} className="form-control" placeholder="ADD ITEM IN THE LIST" aria-label="Recipient's username" aria-describedby="button-addon2" />
   
@@ -28,7 +28,7 @@ function App() {
            return <li className="text-monospace" style={{textAlign:'left' ,marginBottom:'2em', border:'3px solid black', backgroundColor:'#EDF5E1'}}><button style={{backgroundColor:'#8EE4AF', border:'3px solid brown'}} className='btn  btn-xs pull-right'  onClick={()=>{deleteitem(key)}}>DELETE</button><br />{i}</li> 
          })
        }
-     </ul>
+     </ul>DDD dd
      </div>
    
 
