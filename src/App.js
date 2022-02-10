cb62c6dd0d473fc6b75aa7eeab34a68e07e3c6f2
Ravish Kumar garg <ravishgarg8@gmail.com>
2022-02-10 08:44:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [data, setdata] = useState('')
+  const [state, setstate] = useState([])
+  const add =()=>{
+        setstate([...state,data]);
+        setdata('');
+  }
+  const eventHandler=(event)=>{
+    setdata(event.target.value)
+  }
+  const deleteitem=(key)=>{
+    setstate(state.filter((s,k)=>{return k!==key}))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 style={{color:'#05386B', textAlign:'center'}}> To-DO List</h1>
+    <div className="input-group mb-3">
+  <input type="text" onChange={eventHandler} value={data} className="form-control" placeholder="ADD ITEM IN THE LIST" aria-label="Recipient's username" aria-describedby="button-addon2" />
+  
+  <button onClick={add} className="btn btn-outline-secondary " style={{backgroundColor:'brown', color:'white', marginLeft:'1em'}} type="button" id="button-addon2">+</button>
+  </div>
+     <ul style={{listStyleType:'square'}}>
+       {
+         state.map((i,key)=>{
+           return <li className="text-monospace" style={{textAlign:'left' ,marginBottom:'2em', border:'3px solid black', backgroundColor:'#EDF5E1'}}><button style={{backgroundColor:'#8EE4AF', border:'3px solid brown'}} className='btn  btn-xs pull-right'  onClick={()=>{deleteitem(key)}}>DELETE</button><br />{i}</li> 
+         })
+       }
+     </ul>
+     </div>
+   
+
   );
 }
 
